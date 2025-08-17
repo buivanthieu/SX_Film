f49904b69c703920129326fb67b13970d7e820c5
buivanthieu <buivanthieu108@gmail.com>
2025-08-17 16:46:37 +0700

    update api

---
+++ backend/WebBackend/Repositories/Actors/ActorRepository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebBackend.Datas;
+using WebBackend.Models;
+
+namespace WebBackend.Repositories.Actors
+{
+    public class ActorRepository : IActorRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public ActorRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task AddActor(Actor actor)
+        {
+            _context.Actors.Add(actor);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteActor(int id)
+        {
+            var actor = await _context.Actors.FindAsync(id)
+                ?? throw new KeyNotFoundException();
+            _context.Actors.Remove(actor);
+        }
+
+        public async Task<Actor?> GetActorById(int id)
+        {
+            var actor = await _context.Actors.FindAsync(id);
+            return actor;
+        }
+
+        public async Task<ICollection<Actor>> GetActorsByFilmId(int filmId)
+        {
+            var actorList = await _context.Actors
+                .Where(a => a.FilmActors.Any(fa => fa.FilmId == filmId))
+                .ToListAsync();
+            return actorList;
+        }
+
+        public async Task<ICollection<Actor>> GetAllActors()
+        {
+            var actorList = await _context.Actors.ToListAsync();
+            return actorList;
+        }
+
+        public async Task UpdateActor(Actor actor)
+        {
+            _context.Actors.Update(actor);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ backend/WebBackend/Repositories/Bookmarks/BookmarkRepository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebBackend.Datas;
+using WebBackend.Models;
+
+namespace WebBackend.Repositories.Bookmarks
+{
+    public class BookmarkRepository : IBookmarkRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public BookmarkRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task AddBookmark(Bookmark bookmark)
+        {
+            _context.Bookmarks.Add(bookmark);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> CheckExistBookmark(int userId, int filmId)
+        {
+            return await _context.Bookmarks
+                .AnyAsync(b => b.UserId == userId && b.FilmId == filmId);
+        }
+
+        public async Task DeleteBookmark(int id)
+        {
+            var bookmark = await _context.Bookmarks.FindAsync(id);
+            if (bookmark != null)
+            {
+                _context.Bookmarks.Remove(bookmark);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<Bookmark?> GetBookmarkById(int id)
+        {
+            return await _context.Bookmarks
+                .Include(b => b.Film)
+                .FirstOrDefaultAsync(b => b.Id == id);
+        }
+
+        public async Task<ICollection<Bookmark>> GetBookmarksByUserId(int userId)
+        {
+            return await _context.Bookmarks
+                .Where(b => b.UserId == userId)
+                .Include(b => b.Film) 
+                .ToListAsync();
+        }
+
+        
+    }
+}
