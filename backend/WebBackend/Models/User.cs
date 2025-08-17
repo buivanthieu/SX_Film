645c3e25e2cc003c5d785c9526a810fa13a234e2
buivanthieu <buivanthieu108@gmail.com>
2025-08-17 16:46:37 +0700

    update api

--- backend/WebBackend/Models/User.cs
+++ backend/WebBackend/Models/User.cs
@@ -21,6 +21,7 @@
         public ICollection<Rating> Ratings { get; set; } = new List<Rating>();
         public ICollection<Report> Reports { get; set; } = new List<Report>();
         public ICollection<Bookmark> Bookmarks { get; set; } = new List<Bookmark>();
+        public ICollection<BookmarkActor> BookmarkActors { get; set; } = new List<BookmarkActor>();
         public ICollection<Notification> Notifications { get; set; } = new List<Notification>();
         public ICollection<SearchLog> SearchLogs { get; set; } = new List<SearchLog>();
         public ICollection<WatchHistory> WatchHistories { get; set; } = new List<WatchHistory>();
